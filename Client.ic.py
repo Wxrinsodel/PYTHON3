ef47393d07ea02ece6385670e959be9088fef640
Wxrinsodel <pinpinahgase@gmail.com>
2025-01-15 23:47:50 +0700

    How to make it more beautiful????

--- Client.ic.py
+++ Client.ic.py
@@ -1,27 +1,32 @@
 import socket
+import threading
 
 HOST = '127.0.0.1'
-PORT = 1234
+PORT = 12342
 
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def send_message_function(client_socket):
+    while True:
+        message = input("")
+        client_socket.send(message.encode())
+
+# Connect to the server
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.connect((HOST, PORT))
     print("Connected to server")
-except OSError as msg:
-    print(f"Error: {msg}")
-    exit(1)
 
-with s:
+    # Sending the nickname
+    nickname = input("Enter your nickname: ")
+    s.send(nickname.encode())
+
+    # Start a thread for sending messages
+    send_thread = threading.Thread(target=send_message_function, args=(s,))
+    send_thread.start()
+
+    # Main loop to receive messages
     while True:
-        message_to_send = input("You (Client): ")
-        s.send((message_to_send + "\n").encode())
-
-        data = s.recv(1024) 
-        if not data:
-            print("Connection closed by server")
+        try:
+            message_received = s.recv(1024).decode()
+            print(message_received)  # Display the message with the sender's nickname
+        except:
+            print("Disconnected from server")
             break
-
-        message_received = data.decode().strip()
-        print(f"Server: {message_received}")
-
-print("Client finished")
